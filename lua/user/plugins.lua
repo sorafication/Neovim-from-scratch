bb1a3ae89b0375e8a2e48032eee1c6cc4d801b95
Oktavius Wiesner <oktavius.wiesner@cbc.de>
2022-03-16 11:40:48 +0100

    Change Keybindings to work with MacOS

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,9 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- Scrollbar
+  use 'dstein64/nvim-scrollview'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
